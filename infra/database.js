a35a36b85c07ef53b40c7bd5619cb31cadacc640
Marcone Lentz Boff <157663360+m4rcone@users.noreply.github.com>
2024-12-06 17:20:40 +0000

    fix variable name

--- infra/database.js
+++ infra/database.js
@@ -15,7 +15,7 @@ async function query(queryObject) {
 
 async function getNewClient() {
   const client = new Client({
-    host: process.env.POSTGRES_LOCALHOST,
+    host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
